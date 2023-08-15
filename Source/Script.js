3451d6e0d10a5515919bee436108e1c4990b7791
Acerbar <114619445+Acerbar@users.noreply.github.com>
2023-08-15 16:06:03 +0300

    Add files via upload

---
+++ Source/Script.js
@@ -0,0 +1,68 @@
+"use strict"
+
+
+/* INTRO BLOCKS ANIMATION*/
+
+const block_1 = document.getElementById('ib1');
+const block_2 = document.getElementById('ib2');
+const block_3 = document.getElementById('ib3');
+if(window.innerWidth > 1129.99){
+    setTimeout(function(){block_1.classList.add("intro__block_active")}, 2800);
+    setTimeout(function (){ block_2.classList.add("intro__block_active") }, 2000);
+    setTimeout(function (){ block_3.classList.add("intro__block_active") }, 1200);
+}
+else{
+    setTimeout(function () { block_1.classList.add("intro__block_active") }, 1200);
+    setTimeout(function () { block_2.classList.add("intro__block_active") }, 1200);
+    setTimeout(function () { block_3.classList.add("intro__block_active") }, 1200);
+}
+
+/* BURGER MENU */
+
+const burger = document.querySelector('.burger');
+const navBurger = document.querySelector(".burger__nav");
+
+
+document.addEventListener('click', function(event){
+    if(event.target.closest('.burger')){
+        burger.classList.toggle('burger__active');
+        navBurger.classList.toggle('burger__visible');
+        document.body.classList.toggle('_locked');
+    }
+    if(!event.target.closest('.burger__menu')){
+        burger.classList.remove('burger__active');
+        navBurger.classList.remove('burger__visible');
+        document.body.classList.remove('_locked');
+    }
+})
+
+
+/* SWIPER */
+
+new Swiper('.about__photo', {
+    navigation: {
+        nextEl: '.about-arrow_right',
+        prevEl: '.about-arrow_left'
+    },
+    pagination: {
+        el: '.about-pagination',
+        type: "progressbar",
+    },
+    grabCursor: true,
+    autoHeight: true,
+    keyboard: {
+        enabled: true,
+        onlyInViewport: true
+    },
+    loop: true,
+});
+
+/* SERVICE LINKS */
+
+const tabLinks = document.querySelectorAll('.tab__link');
+tabLinks.forEach(item => {
+    item.addEventListener('click', (e) => {
+        tabLinks.forEach(el => { el.classList.remove('tab__link_active'); });
+        item.classList.add('tab__link_active')
+    })
+})
